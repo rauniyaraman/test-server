f1d0e29918e11d72c3ca41938ae21b0dc953fbfc
rauniyaraman <amanrauniyar4545@gmail.com>
2024-09-10 13:58:01 +0545

    minor changes

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World Aman Rauniyar!')
+app.get('/amit', (req, res) => {
+  res.send('Hello World Amit Mandal aaaa!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-aaa
